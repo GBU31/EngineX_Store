3a47305e497a2f7a6b99991d8c42bf029af7a6f1
brookehorizon <danieltashman17@gmail.com>
2022-06-22 10:31:29 +0300

    Updated

--- main/models.py
+++ main/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 
 class item(models.Model):
-    url = models.CharField(max_length=100)
+    url = models.CharField(max_length=500)
     youtube_vid = models.CharField(max_length=100, blank=True)
-    pic_url = models.CharField(max_length=100)
+    pic_url = models.CharField(max_length=500)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     
