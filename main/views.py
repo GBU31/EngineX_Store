412bccc9085bcb23424d1ed30114cd4dbb7da93f
brookehorizon <danieltashman17@gmail.com>
2022-06-24 11:07:38 +0300

    Updated

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import *
+from django.contrib.auth import authenticate, login, logout
 
 
 def home(request):
@@ -7,5 +8,19 @@ def home(request):
     return render(request, 'home.html', {'items':items})
 
 
-def login(request):
-    ''
+def Login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'msg':'your username or password is incorrect'})
+
+    return render(request, 'login.html')
+
+def Logout(request):
+    logout(request)
+    return redirect('home')    
