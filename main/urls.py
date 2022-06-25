2ef7229fd7bd065e705451f30f9aadf3300f1971
brookehorizon <danieltashman17@gmail.com>
2022-06-25 11:55:11 +0300

    Updated

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login', views.Login, name='lgout'),
-    path('logout', views.Logout),
+    path('login', views.Login, name='login'),
+    path('logout', views.Logout, name='logout'),
+    path('signup', views.signup, name='signup'),
+    path('myassets', views.myassets, name='myassets'),
     
 ]
