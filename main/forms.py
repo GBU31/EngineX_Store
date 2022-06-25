d2a1727c601a191e8f7ce7f4a60296960b7330af
brookehorizon <danieltashman17@gmail.com>
2022-06-25 11:54:57 +0300

    Updated

--- main/forms.py
+++ main/forms.py
@@ -1,2 +1,24 @@
 from django import  forms
 from .models import *
+
+
+class Create(forms.ModelForm):
+    class Meta:
+        fields = ['url', 'pic_url', 'title', 'description' ]
+        model = item
+
+        widgets = {
+            'url': forms.TextInput(attrs={'class':"input is-large", "style":'width: 300px; margin-bottom:10px', 'placeholder':'source'}),
+            'pic_url': forms.TextInput(attrs={'class':"input is-large", "style":'width: 300px; margin-bottom:10px', 'placeholder':'picture'}),
+            'title': forms.TextInput(attrs={'class':"input is-large", "style":'width: 300px; margin-bottom:10px', 'placeholder':'title'}),
+            'description': forms.Textarea(attrs={'class':"textarea", 'rows':
+            '3',"style":'width: 300px; margin-bottom:10px', 'placeholder':'description'}),
+        }
+
+        labels = {
+            'url':'',
+            'pic_url':'',
+            'title':'',
+            'description':
+            '',
+        }
