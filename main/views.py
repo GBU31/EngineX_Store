76fbc634f953ee7fd7dd8f769bdb2c8311d43109
brookehorizon <danieltashman17@gmail.com>
2022-06-25 11:55:15 +0300

    Updated

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
 from .models import *
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .forms import *
 
 
 def home(request):
@@ -24,3 +26,30 @@ def Login(request):
 def Logout(request):
     logout(request)
     return redirect('home')    
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'signup.html', {'form': form})
+
+
+def myassets(request):
+    form = Create()
+
+    if request.method == 'POST':
+        form = Create(request.POST)
+        if form.is_valid():
+            form2 = form.save(commit=False)
+            form2.user = request.user
+            form2.save()
+
+
+    items = item.objects.all()
+    return render(request, 'myassets.html', {'items':items, 'form':form})
